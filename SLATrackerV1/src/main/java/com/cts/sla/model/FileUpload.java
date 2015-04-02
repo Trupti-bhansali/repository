e73704787ae69d581bdabb1628677f9eb933832b
Trupti-bhansali <trupsgh.99@gmail.com>
2015-04-02 16:46:45 +0530

    SLATracker:  -Added new file

---
+++ SLATrackerV1/src/main/java/com/cts/sla/model/FileUpload.java
@@ -0,0 +1,17 @@
+package com.cts.sla.model;
+ 
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+ 
+public class FileUpload {
+ 
+    private List<MultipartFile> uploadFiles;
+ 
+    public List<MultipartFile> getFiles() {
+        return uploadFiles;
+    }
+ 
+    public void setFiles(List<MultipartFile> files) {
+        this.uploadFiles = files;
+    }
+}
